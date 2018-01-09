e47de72e0a5d91219b57b066ca7b2500677029bd
Pavel Roschin <rpg0889@gmail.com>
2018-01-10 00:50:14 +0300

    Added pure C/X11 screenshot example

---
+++ c/X11Screenshot.c
@@ -0,0 +1,48 @@
+/*
+Probably simplest screenshot application
+gcc -O2 -o X11Screenshot X11Screenshot.c -lX11
+./X11Screenshot > screenshot.ppm
+*/
+
+#include <X11/Xlib.h>
+#include <X11/X.h>
+#include <stdio.h>
+
+int main()
+{
+	Display *display = XOpenDisplay(NULL);
+	if (display == NULL)
+		return -1;
+	Window root = DefaultRootWindow(display);
+	XWindowAttributes gwa;
+
+	XGetWindowAttributes(display, root, &gwa);
+	XImage *image = XGetImage(display, root, 0, 0, gwa.width, gwa.height,
+	                          AllPlanes, ZPixmap);
+	if (image == NULL)
+		return -1;
+
+	/* Generate PPM header */
+	puts("P6");
+	printf("%d %d\n", image->width, image->height);
+	puts("255");
+
+	int bpp = image->bits_per_pixel / 8;
+	char line[image->width * 3];
+
+	for (int y = 0; y < image->height; y++)
+	{
+		for (int x = 0; x < image->width; x++)
+		{
+			unsigned char *c =
+				image->data + x * bpp + y * image->width * bpp;
+			line[x * 3 + 0] = *(c + 2);
+			line[x * 3 + 1] = *(c + 1);
+			line[x * 3 + 2] = *(c + 0);
+		}
+		fwrite(line, 1, sizeof(line), stdout);
+
+	}
+
+	return 0;
+}
