0de0b9d65e586adc2db46ae49a62c730b6cbff2b
Pavel Roschin <rpg0889@gmail.com>
2018-03-26 15:18:44 +0300

    RN - Image shadow

--- react-native/BottomNavigationTest/App.js
+++ react-native/BottomNavigationTest/App.js
@@ -1,15 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <Image source={require("./shadow.png")} style={styles.shadowTop} />
+        <ScrollView>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu....</Text>
+        </ScrollView>
+        <Image source={require("./shadow.png")} style={styles.shadow} />
+
         <View style={styles.tabBar}>
-          <Text>Test</Text>
+          <Text style={{ fontSize: 10 }}>Test</Text>
         </View>
       </View>
     );
@@ -19,20 +57,41 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  shadow: {
+    // bottom: 56,
+    height: 10,
+    marginTop: -10,
+    // position: "absolute",
+    width: "100%",
+    resizeMode: "stretch",
+    opacity: 0.2,
+    // transform: [{ rotateX: "180deg" }],
+  },
+  shadowTop: {
+    // bottom: 56,
+    height: 26,
+    marginBottom: -26,
+    // position: "absolute",
+    width: "100%",
+    resizeMode: "stretch",
+    opacity: 0.8,
+    // blurradius: 10,
+    transform: [{ rotateX: "180deg" }],
   },
   tabBar: {
-    flex: 1,
-    backgroundColor: '#fff',
-    position: "absolute",
-    bottom: 0,
+    flex: 0,
+    backgroundColor: "#fff",
+    // position: "absolute",
+    // bottom: 0,
     height: 56,
-    elevation: 8,
+    // elevation: 8,
     width: "100%",
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     opacity: 1
-  },
+  }
 });
