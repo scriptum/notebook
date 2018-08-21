d15087f651354f086528ebe709a711d83af3c20b
Pavel Roschin <rpg0889@gmail.com>
2018-08-21 20:33:01 +0300

    Select removed range

--- vscode/scripts/remove-bracket-pair.js
+++ vscode/scripts/remove-bracket-pair.js
@@ -7,7 +7,7 @@ exports.execute = async function(args) {
   if (!editor) return 666;
   await vscode.commands.executeCommand('editor.action.jumpToBracket');
   let pos1 = editor.selection.start;
-  await  vscode.commands.executeCommand('editor.action.jumpToBracket');
+  await vscode.commands.executeCommand('editor.action.jumpToBracket');
   let pos2 = editor.selection.start;
   if (pos1.isEqual(pos2)) {
     return 666;
@@ -21,5 +21,7 @@ exports.execute = async function(args) {
     editBuilder.delete(new vscode.Range(pos1, pos1.translate(0, 1)));
     editBuilder.delete(new vscode.Range(pos2, pos2.translate(0, 1)));
   });
+  if (pos1.line == pos2.line) pos2 = pos2.translate(0, -1);
+  editor.selection = new vscode.Selection(pos1, pos2);
   return 666;
 };
