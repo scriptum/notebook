de615968697e9aff316c3bdea2f33f03a7e13709
Pavel Roschin <rpg0889@gmail.com>
2018-08-21 20:14:55 +0300

    VSCode script - remove pair of brackets

---
+++ vscode/scripts/remove-bracket-pair.js
@@ -0,0 +1,25 @@
+// Remove pair of brackets
+// https://mkloubert.github.io/vs-script-commands/index.html
+
+exports.execute = async function(args) {
+  const vscode = require('vscode');
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) return 666;
+  await vscode.commands.executeCommand('editor.action.jumpToBracket');
+  let pos1 = editor.selection.start;
+  await  vscode.commands.executeCommand('editor.action.jumpToBracket');
+  let pos2 = editor.selection.start;
+  if (pos1.isEqual(pos2)) {
+    return 666;
+  }
+  if (pos1.isAfter(pos2)) {
+    let pos3 = pos1;
+    pos1 = pos2;
+    pos2 = pos3;
+  }
+  editor.edit(editBuilder => {
+    editBuilder.delete(new vscode.Range(pos1, pos1.translate(0, 1)));
+    editBuilder.delete(new vscode.Range(pos2, pos2.translate(0, 1)));
+  });
+  return 666;
+};
