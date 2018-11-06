46f7bb9d6b455f1b6268d72b7779188601cc9665
Pavel Roschin <rpg0889@gmail.com>
2018-11-06 23:47:17 +0300

    Kotlin fib implementation

--- kotlin/fib/src/main/kotlin/com/example/fib/FibController.kt
+++ kotlin/fib/src/main/kotlin/com/example/fib/FibController.kt
@@ -1,2 +1,13 @@
 package com.example.fib
 
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class FibController {
+    private fun fibonacci(n: Long): Long = if (n <= 1) n else fibonacci(n - 1) + fibonacci(n - 2)
+
+    @RequestMapping(value = ["/{number}"])
+    fun fib(@PathVariable(value = "number", required = true) number: Long) = fibonacci(number)
+}
