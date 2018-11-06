c7387b6ac68c171ace80733529d491fa5661e96b
Pavel Roschin <rpg0889@gmail.com>
2018-11-06 23:47:58 +0300

    Update navigation test

--- react-native/BottomNavigationTest/App.js
+++ react-native/BottomNavigationTest/App.js
@@ -1,97 +1,56 @@
-import React from "react";
-import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
+// @flow
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-      <Image source={require("./shadow.png")} style={styles.shadowTop} />
-        <ScrollView>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu....</Text>
-        </ScrollView>
-        <Image source={require("./shadow.png")} style={styles.shadow} />
+import React from 'react';
+import {StyleSheet, Text, View, ScrollView, Image, Button} from 'react-native';
+import {TabNavigator, StackNavigator} from 'react-navigation';
+import Lorem from './Components/Lorem';
+import TabBar from './Components/TabBar';
 
-        <View style={styles.tabBar}>
-          <Text style={{ fontSize: 10 }}>Test</Text>
-        </View>
-      </View>
-    );
-  }
-}
+// const Lorem = () => <Lorem />;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "space-between"
-  },
-  shadow: {
-    // bottom: 56,
-    height: 10,
-    marginTop: -10,
-    // position: "absolute",
-    width: "100%",
-    resizeMode: "stretch",
-    opacity: 0.2,
-    // transform: [{ rotateX: "180deg" }],
-  },
-  shadowTop: {
-    // bottom: 56,
-    height: 26,
-    marginBottom: -26,
-    // position: "absolute",
-    width: "100%",
-    resizeMode: "stretch",
-    opacity: 0.8,
-    // blurradius: 10,
-    transform: [{ rotateX: "180deg" }],
-  },
-  tabBar: {
-    flex: 0,
-    backgroundColor: "#fff",
-    // position: "absolute",
-    // bottom: 0,
-    height: 56,
-    // elevation: 8,
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    opacity: 1
-  }
+const HomeScreen = (props: Object) => (
+  <View styel={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <ScrollView>
+      <Text>{JSON.stringify(props)}</Text>
+    </ScrollView>
+
+    <Button
+      title='Go to Details'
+      onPress={() => props.navigation.navigate('Details')}
+    />
+  </View>
+);
+
+const SettingsScreen = (props: Object) => (
+  <View syle={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <ScrollView>
+      <Text>{JSON.stringify(props)}</Text>
+    </ScrollView>
+    <Button
+      title='Go to Details'
+      onPress={() => props.navigation.navigate('Details')}
+    />
+  </View>
+);
+
+const HomeStack = StackNavigator({
+  Home: {screen: HomeScreen},
+  Details: {screen: Lorem},
 });
+
+const SettingsStack = StackNavigator({
+  Settings: {screen: SettingsScreen},
+  Details: {screen: Lorem},
+});
+
+export default TabNavigator(
+  {
+    Home: {screen: HomeStack},
+    Settings: {screen: SettingsStack},
+  },
+  {
+    // tabBarComponent: TabBar,
+    animationEnabled: false,
+    lazy: false,
+  },
+);
