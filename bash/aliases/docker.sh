a711b5d9e37049927260f8473bae82df4866ddbd
Pavel Roschin <rpg0889@gmail.com>
2019-03-07 13:23:23 +0300

    Docker-compose aliases

---
+++ bash/aliases/docker.sh
@@ -0,0 +1,6 @@
+alias dcs='docker-compose start'
+alias dcu='docker-compose up -d'
+alias dcr='docker-compose restart'
+alias dcd='docker-compose stop'
+alias dcrm='docker-compose rm'
+alias dc='docker-compose'
