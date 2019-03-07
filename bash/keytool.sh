978e5de40d9adebb5cb8524f7ae32c3af76ea0bd
Pavel Roschin <rpg0889@gmail.com>
2019-03-07 13:18:03 +0300

    Keytool - mac support

--- bash/keytool.sh
+++ bash/keytool.sh
@@ -1,15 +1,34 @@
-keytool.add-trusted-cert(){
-  local keystore=/etc/pki/java/cacerts storepass=changeit
-  for f in "$@"; do
-    sudo keytool -keystore "$keystore" -storepass "$storepass" \
-    -importcert -alias "${f%.*}" -trustcacerts -file "$f"
+keytool.find-keystore(){
+  local stores=(
+    /etc/pki/java/cacerts
+    /Library/Java/JavaVirtualMachines/*/Contents/Home/jre/lib/security/cacerts
+    /Library/Java/JavaVirtualMachines/*/Contents/Home/lib/security/cacerts
+  )
+  for s in "${stores[@]}"; do
+    if [[ -f "$s" ]]; then
+      echo "$s"
+      return
+    fi
   done
 }
 
-keytool.delete(){
-  local keystore=/etc/pki/java/cacerts storepass=changeit
+keytool.add-trusted-cert(){
+  local keystore=$(keytool.find-keystore) storepass=changeit
+  for f in "$@"; do
+    sudo keytool -keystore "$keystore" -storepass "$storepass" \
+    -importcert -alias "${f%.*}" -trustcacerts -noprompt -file "$f"
+  done
+}
+
+keytool.delete-cert(){
+  local keystore=$(keytool.find-keystore) storepass=changeit
   for alias in "$@"; do
     sudo keytool -keystore "$keystore" -storepass "$storepass" \
     -delete -alias "$alias"
   done
+}
+
+keytool.list(){
+  local keystore=$(keytool.find-keystore) storepass=changeit
+  keytool -keystore "$keystore" -storepass "$storepass" -list | awk '$1!="Certificate"{print $1}'
 }
