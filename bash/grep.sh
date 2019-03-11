9178addf573a965bb03fd2ce16117c7bd5d5255a
Pavel Roschin <rpg0889@gmail.com>
2019-03-11 20:19:27 +0300

    Fix grep on Mac

--- bash/grep.sh
+++ bash/grep.sh
@@ -1,6 +1,11 @@
+if hash ggrep 2>/dev/null; then
+  _GREP=ggrep
+else
+  _GREP=/bin/grep
+fi
 # search anything in c/c++ source
 src() {
-  /bin/grep --color=always -InRswa \
+  $_GREP --color=always -InRswa \
   '--include=*.'{[ch]{,pp,xx,++},h[hp],c[cp],tcc,[CH],CPP} \
   "$@" . 2> /dev/null | less -XFRS
 }
@@ -8,25 +13,25 @@ alias grep.c=src
 
 # search anything in python source
 srp() {
-  /bin/grep --color=always -InRswa '--include=*.py' "$@" . 2> /dev/null | less -XFRS
+  $_GREP --color=always -InRswa '--include=*.py' "$@" . 2> /dev/null | less -XFRS
 }
 alias grep.py=srp
 
 # search anything in nim source
 srn() {
-  /bin/grep --color=always -iInRswa '--include=*.nim' "$@" . 2> /dev/null | less -XFRS
+  $_GREP --color=always -iInRswa '--include=*.nim' "$@" . 2> /dev/null | less -XFRS
 }
 alias grep.nim=srn
 
 # search in assembler
 srs() {
-  /bin/grep --color=always -InRsa '--include=*.s' "$@" . 2> /dev/null | less -XFRS
+  $_GREP --color=always -InRsa '--include=*.s' "$@" . 2> /dev/null | less -XFRS
 }
 alias grep.asm=srs
 
 # search in javascript
 srj() {
-  /bin/grep --color=always --exclude-dir=node_modules --exclude-dir=.git --exclude-dir=.svn -InRswa '--include=*.js' "$@" . 2> /dev/null | less -XFRS
+  $_GREP --color=always --exclude-dir=node_modules --exclude-dir=.git --exclude-dir=.svn -InRswa '--include=*.js' "$@" . 2> /dev/null | less -XFRS
 }
 alias grep.js=srj
 
@@ -38,16 +43,20 @@ sr() {
     inc+="--include=$1"
     shift
   done
-  /bin/grep --color=always -iInRs --exclude-dir=.git --exclude-dir=.svn --exclude-dir=.idea  --exclude-dir=.vscode "${inc[@]}" "$@" . 2> /dev/null | less -XFRS
+  $_GREP --color=always -iInRs --exclude-dir=.git --exclude-dir=.svn --exclude-dir=.idea  --exclude-dir=.vscode "${inc[@]}" "$@" . 2> /dev/null | less -XFRS
 }
 
+if hash isoinfo 2>/dev/null; then
 grep.iso() {
   { isoinfo -fJ -i "$1" 2> /dev/null || isoinfo -fR -i "$1"; } | \
-    grep -e rpm$ -e deb$ | sed s@.*/@@ | grep "$2"
+    grep -e 'rpm$' -e 'deb$' | sed s@.*/@@ | grep "$2"
 }
+fi
 
+if [[ -d ~/.Skype ]]; then
 grep.skype() {
   local db=$(ls ~/.Skype/*/main.db | head -1)
   sqlite3 -separator " " "$db" "select datetime(timestamp, 'unixepoch', 'localtime'), author, body_xml from messages;" | \
     grep -i "$@" | less -XFRS
 }
+fi
